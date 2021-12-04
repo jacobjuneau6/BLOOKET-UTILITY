7ecd5949e87a4fa0859d5c8fb9c5651d88899feb
jacobjuneau6 <79861515+jacobjuneau6@users.noreply.github.com>
2021-12-04 05:05:12 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 
+setPref('coolInjectables', {myPref: true})
+setFormat('coolInjectables/myPref', 'My Pref')
+ModMenu.init
+await ModMenu.init()
+ModMenu.setShowing
+ModMenu.menu
+ModMenu.MenuTree
+ModMenu.menu.arr.push(new ModMenu.MenuTree('Cool Injectables', []))
+menuTree.name
+ModMenu.MenuTree
+ModMenu.MenuBool
+ModMenu.menu.arr.push(new ModMenu.MenuBool('Cool Bool', true))
